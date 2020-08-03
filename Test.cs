4e7e7df9dd6e0c8289ca15d840e1f1c8940794ec
hiroto_takizawa <takky0131@gmail.com>
2020-08-03 23:21:40 +0900

    lesson4の課題と発展課題です。 よろしくお願いいたします。

---
+++ Test.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] array = new int[5];
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
+
+        for (int i = 4; i > 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+
+
+
